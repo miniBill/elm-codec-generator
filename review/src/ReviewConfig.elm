1f40c8310b52608fabb633b0ae9c375a6a4cf75c
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2023-08-30 21:21:17 +0200

    Update elm-review rules

--- review/src/ReviewConfig.elm
+++ review/src/ReviewConfig.elm
@@ -1,12 +1,19 @@
 module ReviewConfig exposing (config)
 
+import Docs.ReviewAtDocs
 import NoAlways
 import NoBooleanCase
+import NoConfusingPrefixOperator
+import NoDebug.Log
+import NoDebug.TodoOrToString
 import NoExposingEverything
 import NoImportingEverything
 import NoMissingTypeAnnotation
+import NoMissingTypeAnnotationInLetIn
 import NoMissingTypeExpose
 import NoModuleOnExposedNames
+import NoPrematureLetComputation
+import NoSimpleLetBody
 import NoUnused.CustomTypeConstructorArgs
 import NoUnused.CustomTypeConstructors
 import NoUnused.Dependencies
@@ -15,18 +22,27 @@ import NoUnused.Modules
 import NoUnused.Parameters
 import NoUnused.Patterns
 import NoUnused.Variables
-import Review.Rule exposing (Rule)
+import Review.Rule as Rule exposing (Rule)
+import Simplify
 
 
 config : List Rule
 config =
     [ NoAlways.rule
     , NoBooleanCase.rule
+    , Docs.ReviewAtDocs.rule
+    , NoConfusingPrefixOperator.rule
+    , NoDebug.Log.rule
+    , NoDebug.TodoOrToString.rule
+        |> Rule.ignoreErrorsForDirectories [ "tests/" ]
     , NoExposingEverything.rule
     , NoImportingEverything.rule []
     , NoMissingTypeAnnotation.rule
+    , NoMissingTypeAnnotationInLetIn.rule
     , NoMissingTypeExpose.rule
     , NoModuleOnExposedNames.rule
+    , NoSimpleLetBody.rule
+    , NoPrematureLetComputation.rule
     , NoUnused.CustomTypeConstructorArgs.rule
     , NoUnused.CustomTypeConstructors.rule []
     , NoUnused.Dependencies.rule
@@ -35,4 +51,6 @@ config =
     , NoUnused.Parameters.rule
     , NoUnused.Patterns.rule
     , NoUnused.Variables.rule
+    , Simplify.rule Simplify.defaults
     ]
+        |> List.map (Rule.ignoreErrorsForDirectories [ "codegen/Gen" ])
